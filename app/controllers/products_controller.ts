b163f590613bae0a577fd5052d6cae2e8f66fcc4
Ewiiz <128278275+Ewiiz@users.noreply.github.com>
2024-04-24 15:08:25 +0400

    Product

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -3,14 +3,11 @@ import db from '@adonisjs/lucid/services/db'
 
 export default class ProductsController {
   get3Products() {
-    return db
-      .from('products')
-      .select('id', 'title', 'price', 'description', 'brand', 'url_image')
-      .limit(3)
+    return db.from('products').select('id', 'title', 'price', 'brand', 'url_image').limit(3)
   }
 
   getProducts() {
-    return db.from('products').select('id', 'title', 'price', 'description', 'brand', 'url_image')
+    return db.from('products').select('id', 'title', 'price', 'brand', 'url_image')
   }
 
   async getProduct({ params }: HttpContext) {
--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const CartsController = () => import('#controllers/carts_controller')
 const ProductsController = () => import('#controllers/products_controller')
 
 router.get('/', async () => {
@@ -19,3 +20,8 @@ router.get('/', async () => {
 router.get('accueil', [ProductsController, 'get3Products'])
 router.get('products', [ProductsController, 'getProducts'])
 router.get('products/:id', [ProductsController, 'getProduct'])
+
+router.get('cart', [CartsController, 'index'])
+router.post('cart', [CartsController, 'store'])
+router.patch('cart', [CartsController, 'update'])
+router.delete('cart/:id', [CartsController, 'destroy'])
